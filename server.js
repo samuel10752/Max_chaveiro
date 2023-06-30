5c99243c46566c12412012fd6fd0727763f2eca4
samuel107152 <samuelcauabarbosa114@gmail.com>
2023-06-29 23:43:52 -0300

    teste de api de envio correto pt1

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors'); // Importe o módulo cors
+
+const app = express();
+
+// Use o middleware cors
+app.use(cors());
+// Endpoint para buscar endereço por CEP
+app.get('/endereco/:cep', async (req, res) => {
+  try {
+    const cep = req.params.cep;
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+    if (response.status === 200) {
+      const endereco = {
+        logradouro: response.data.logradouro,
+        bairro: response.data.bairro,
+        cidade: response.data.localidade,
+        estado: response.data.uf,
+        cep: response.data.cep
+      };
+      res.json(endereco);
+    } else {
+      res.status(500).json({ message: 'Erro ao buscar endereço' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar endereço' });
+  }
+});
+
+// Iniciar o servidor na porta 3000
+app.listen(3000, () => {
+  console.log('API de busca de endereço por CEP iniciada na porta 3000');
+});
