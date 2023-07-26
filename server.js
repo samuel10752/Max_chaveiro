fab85437ad955c80317f6ba4798d46c48059a8f1
samuel107152 <samuelcauabarbosa114@gmail.com>
2023-07-26 15:55:14 -0300

    att; part2 sucesso de buscar de frete

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // Importe o módulo cors
+const soap = require('soap');
+const cors = require('cors');
 
 const app = express();
-
-// Use o middleware cors
 app.use(cors());
-// Endpoint para buscar endereço por CEP
+
+// Rota para buscar endereço por CEP
 app.get('/endereco/:cep', async (req, res) => {
   try {
     const cep = req.params.cep;
@@ -26,12 +26,46 @@ app.get('/endereco/:cep', async (req, res) => {
       res.status(500).json({ message: 'Erro ao buscar endereço' });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Erro ao buscar endereço' });
+    res.status(500).json({ message: 'Erro ao buscar endereço', error: error });
   }
-
 });
 
-// Iniciar o servidor na porta 3000
+// Rota para calcular o frete com base no CEP
+app.get('/frete/:cep', async (req, res) => {
+  try {
+    const cep = req.params.cep;
+    const url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
+
+    const args = {
+      nCdEmpresa: '', // Seu código de empresa nos Correios, se houver
+      sDsSenha: '', // Sua senha nos Correios, se houver
+      nCdServico: '04014', // Código do serviço - 04014 é o código para SEDEX à vista
+      sCepOrigem: '37524-000', // CEP de origem
+      sCepDestino: cep, // CEP de destino
+      nVlPeso: '1', // Peso da encomenda em quilogramas
+      nCdFormato: '1', // Formato da encomenda (1 é caixa/pacote, 2 é rolo/prisma, 3 é envelope)
+      nVlComprimento: '20', // Comprimento da encomenda em centímetros
+      nVlAltura: '5', // Altura da encomenda em centímetros
+      nVlLargura: '15', // Largura da encomenda em centímetros
+      nVlDiametro: '0', // Diâmetro da encomenda em centímetros
+      sCdMaoPropria: 'n', // Indica se a encomenda será entregue em mãos ('s' ou 'n')
+      nVlValorDeclarado: '0', // Valor declarado da encomenda, para fins de seguro
+      sCdAvisoRecebimento: 's' // Indica se a encomenda requer aviso de recebimento ('s' ou 'n')
+    };
+
+    soap.createClientAsync(url).then((client) => {
+      return client.CalcPrecoPrazoAsync(args);
+    }).then((result) => {
+      res.json(result[0].CalcPrecoPrazoResult.Servicos.cServico);
+    }).catch((err) => {
+      res.status(500).json({ message: 'Erro ao calcular o frete', error: err });
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao calcular o frete', error: error });
+  }
+});
+
+// Ouvir na porta 3000
 app.listen(3000, () => {
-  console.log('API de busca de endereço por CEP iniciada na porta 3000');
+  console.log('API iniciada na porta 3000');
 });
